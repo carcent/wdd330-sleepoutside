0e1abc7db63a00ec05c18438783ec720bbbb59d0
BuildTools <unconfigured@null.spigotmc.org>
2025-05-17 17:54:38 +0400

    fixed issue related to week 1 activity

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -20,7 +20,7 @@ export default class ProductDetails {
     
     addProductToCart() {
         const cartItems = getLocalStorage("so-cart") || [];
-        cartItems.push(product);
+        cartItems.push(this.product);
         setLocalStorage("so-cart", cartItems);
     }
 
