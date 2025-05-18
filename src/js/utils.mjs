21c5e9fa90557d119bcd9992e239c4e1d02e3593
carcent <carli.centurion@gmail.com>
2025-05-18 00:34:35 -0300

    total$ in cart

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -37,3 +37,19 @@ export function renderListWithTemplate(template, parentElement, list, position =
   }
   parentElement.insertAdjacentHTML(position, htmlString.join(""));
 }
+
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product
+}
+
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
