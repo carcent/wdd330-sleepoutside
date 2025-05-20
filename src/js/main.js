f04ede1d1b356e618695e66768351fb5a478208b
Ethem-deli <del22021@byui.edu>
2025-05-20 10:13:49 +0300

    Alert added to index.html and needed files as js.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import Alert from "./js/Alert.js";
 
 const dataSource = new ProductData("tents");
 
@@ -9,3 +10,6 @@ const listElement = document.querySelector(".product-list");
 //productList.init();
 const myList = new ProductList('tents', dataSource, listElement);
 myList.init();
+
+const alert = new Alert();
+alert.init();
