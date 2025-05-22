12ddcae04196cf71ba431749117bc8d3d9b4a3da
AntiFireWall <ayvazyands@gmail.com>
2025-05-22 21:44:26 +0400

    week 3 Team activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import Alert from "./Alert";
+import {loadHeaderFooter} from "./utils.mjs"
 
 const dataSource = new ProductData("tents");
 const listElement = document.querySelector(".product-list");
@@ -9,3 +10,5 @@ productList.init();
 
 const alert = new Alert();
 alert.init();
+
+loadHeaderFooter();
