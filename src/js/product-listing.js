df3bd9743d8fff7901b4e1fcaa4ba231a1f415fa
AntiFireWall <ayvazyands@gmail.com>
2025-05-26 18:18:05 +0400

    finished Individual 3 activity

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,7 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 
-const dataSource = new ProductData("tents");
+loadHeaderFooter();
+
+const category = getParam("category");
+const dataSource = new ProductData();
 const listElement = document.querySelector(".product-list");
-const productList = new ProductList("Tents", dataSource, listElement);
+const productList = new ProductList(category, dataSource, listElement);
 productList.init();
