f16751ab9bdc46c46d4e15852431b9a01eec76cc
carcent <carli.centurion@gmail.com>
2025-05-29 17:04:13 -0300

    size responsive images

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,11 +1,11 @@
-import { capitalizeFirstLetter, renderListWithTemplate } from "./utils.mjs";
+import { capitalizeFirstLetter, getResponsiveImage, renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
   const isDiscounted = product.FinalPrice < product.SuggestedRetailPrice;
 
   return `<li class="product-card">
     <a href="/product_pages/?product=${product.Id}">
-      <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
+      <img src="${getResponsiveImage(product)}" alt="Image of ${product.Name}">
       <h2 class="card__brand">${product.Brand.Name}</h2>
       <h3 class="card__name">${product.Name}</h3>
       <p class="product-card__price">
